fc496026b5f217343e3b60226e089d976da07538
Emmanuel-Obewu <eleanyaemmanuel5@gmail.com>
2024-08-10 05:11:07 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Heading from './Heading'
+import ListItems from './ListItems';
+
+function App(){
+   return (
+       <>
+   <Heading/>
+    <ListItems/>
+    </>
+   );
+}
+
+export default App;
---
+++ src/ListItems.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ListItem(){
+    return <ul>
+        <li>Bacon</li>
+        <li>Beans</li>
+        <li>D'garrison</li>
+    </ul>
+}
+
+export default ListItem;
